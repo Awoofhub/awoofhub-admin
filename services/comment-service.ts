c661ef50678f0f3a528f98897092ee8b9732e8ce
divine-itura <divine@myitura.com>
2026-08-15 18:30:46 +0100

    finished the user detail page hooks

--- services/comment-service.ts
+++ services/comment-service.ts
@@ -2,16 +2,18 @@ import { apiClient } from "@/lib/api-client";
 import { ApiResponse } from "@/types/api-response";
 import { Comment } from "@/types/comment";
 
-async function getAllComments(page: number = 1, limit: number = 5): Promise<ApiResponse<Comment[]>> {
+export async function getAllComments(page: number = 1, limit: number = 5): Promise<ApiResponse<Comment[]>> {
   const res: ApiResponse<Comment[]> = await apiClient.get("/comments", {
     params: { page, limit, },
   });
 
   return res;
 }
+export async function getCommentsByUser({userid}:{userid:string}): Promise<ApiResponse<Comment[]>> {
+  const res: ApiResponse<Comment[]> = await apiClient.get(`/comments/user/${userid}`, {
+    params: { userid },
+  });
 
-const CommentService = {
-  getAllComments,
-};
+  return res;
+}
 
-export default CommentService;
