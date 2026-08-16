4de6323b6360e6b9f1e34b6da8fe5b994581f887
Moses Martins <mmoses061@gmail.com>
2026-08-16 14:45:27 +0100

    update header and sidebar navigation

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -6,11 +6,11 @@ export default function Header() {
 
     return (
         <header className="sticky top-0 left-0 z-[99] shadow-lg shadow-black/5 relative">
-            <div className="hidden md:flex justify-between items-center bg-primary w-full h-[60px] px-3 xl:px-8">
+            <div className="hidden lg:flex justify-between items-center bg-primary w-full h-[60px] px-3 xl:px-8">
                 <span className="text-white text-2xl">Admin Panel</span>
                 <Dropdown />
             </div>
-            <div className="md:hidden flex justify-between items-center bg-primary h-[60px] px-3">
+            <div className="lg:hidden flex justify-between items-center bg-primary h-[60px] px-3">
                 <Hamburger />
                 <Dropdown />
             </div>
