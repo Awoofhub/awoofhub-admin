3b425ac170eee4e5c58729ba1c862dd1c737c054
Moses Martins <mmoses061@gmail.com>
2026-08-19 06:25:18 +0100

    create user and offers table

--- app/(main)/users/page.tsx
+++ app/(main)/users/page.tsx
@@ -1,5 +1,32 @@
+'use client';
 
-export default function UsersPage() {
-   
-    
+import UserTable from "@/components/users/UserTable";
+import { ChevronRight } from "lucide-react";
+import { use } from "react";
+
+type FilterParams = {
+    search?: string,
+    status?: string;
+};
+
+interface FilterProps {
+    searchParams: Promise<FilterParams>;
+}
+
+export default function UsersPage({ searchParams }: FilterProps) {
+    const params = use(searchParams);
+    const { search, status } = params;
+
+    return (
+        <div className="p-4">
+            <div className="my-4 flex items-center gap-1 text-xl text-black font-baloo font-semibold">
+                <ChevronRight size={18} className="hidden xs:inline" />
+                <span>All Users</span>
+            </div>
+
+            <UserTable search={search} status={status} />
+        </div>
+    )
+
+
 }
--- components/offers/OfferColumns.tsx
+++ components/offers/OfferColumns.tsx
@@ -2,31 +2,37 @@ import { Offer } from "@/types/offer";
 import { formatDateTime } from "@/utils/formatDateTime";
 import { AlertTriangle, Clock } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
+import OfferModal from "../modals/OfferModal";
 import { Column } from "../table/Table";
 
-function ExpiryBadge({ isExpired }: { isExpired: boolean }) {
+
+function OfferActions({ offer }: { offer: Offer }) {
+    const [openModal, setOpenModal] = useState(false);
+
     return (
-        <div
-            className={`flex items-center gap-1.5 px-3 py-1 rounded-full border text-[10px] uppercase tracking-wider font-bold w-fit ${isExpired
-                ? "bg-gray-50 text-gray-500 border-gray-200"
-                : "bg-blue-50 text-blue-600 border-blue-100"
-                }`}
-        >
-            {isExpired ? (
-                <>
-                    <AlertTriangle className="w-3 h-3" />
-                    Expired
-                </>
-            ) : (
-                <>
-                    <Clock className="w-3 h-3" />
-                    Valid
-                </>
-            )}
-        </div>
+        <>
+            <button
+                type="button"
+                className="flex justify-center cursor-pointer p-2"
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setOpenModal(true);
+                }}
+            >
+                <BsThreeDots size={20} />
+            </button>
+
+            <OfferModal
+                offerId={offer.id}
+                isOpen={openModal}
+                onClose={() => setOpenModal(false)}
+            />
+        </>
     );
-}
+};
+
 
 export const OfferColumns: Column<Offer>[] = [
     {
@@ -89,7 +95,27 @@ export const OfferColumns: Column<Offer>[] = [
         render: (offer) => {
             const isExpired = new Date(offer.endDate) < new Date();
 
-            return <ExpiryBadge isExpired={isExpired} />;
+            return (
+                <div
+                    className={`flex items-center gap-1.5 px-3 py-1 rounded-full border text-[10px] uppercase tracking-wider font-bold w-fit ${isExpired
+                        ? "bg-gray-50 text-gray-500 border-gray-200"
+                        : "bg-blue-50 text-blue-600 border-blue-100"
+                        }`}
+                >
+                    {isExpired ? (
+                        <>
+                            <AlertTriangle className="w-3 h-3" />
+                            Expired
+                        </>
+                    ) : (
+                        <>
+                            <Clock className="w-3 h-3" />
+                            Valid
+                        </>
+                    )}
+                </div>
+            )
+
         },
     },
 
@@ -97,11 +123,6 @@ export const OfferColumns: Column<Offer>[] = [
         key: "actions",
         header: "Actions",
         className: "text-center",
-        render: () =>
-        (
-            <div className="flex justify-center">
-                <BsThreeDots size={20} />
-            </div>
-        )
+        render: (offer) => <OfferActions offer={offer} />,
     },
 ];
